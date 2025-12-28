cc44c6a9ffe24e0b743b37507626a848fab2bd91
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2025-12-28 23:31:17 +0100

    add more pride presets

--- src/consts.js
+++ src/consts.js
@@ -4,7 +4,16 @@ export const accentColor = "#CAE6FF";
 // Preset colors
 export const presetColors = {
     "Pride": ["#E40303", "#FF8C00", "#FFED00", "#008026", "#004CFF", "#732982"],
+    "Ally": ["#000000", "#FFFFFF", "#F00000", "#FFFF00", "#007A41", "#4041FE", "#A001BE"],
+    "Lesbian": ["#D52D00", "#EF7627", "#FF9A56", "#FFFFFF", "#D162A4", "#B55690", "#A30262"],
+    "Gay Men": ["#078D70", "#26CEAA", "#98E8C1", "#FFFFFF", "#7BADE2", "#5049CC", "#3D1A78"],
     "Transgender": ["#5BCEFA", "#F5A9B8", "#FFFFFF", "#F5A9B8", "#5BCEFA"],
+    "Bisexual": ["#D60270", "#9B4F96", "#0038A8"],
+    "Asexual": ["#000000", "#A3A3A3", "#FFFFFF", "#800080"],
     "Pansexual": ["#FF218C", "#FFD800", "#21B1FF"],
-    "Ally": ["#000000", "#FFFFFF", "#F00000", "#FFFF00", "#007A41", "#4041FE", "#A001BE"]
+    "Aromantic": ["#3DA542", "#A7D379", "#FFFFFF", "#A9A9A9", "#000000"],
+    "Genderfluid": ["#FF76A4", "#FFFFFF", "#C011D7", "#000000", "#2F3CBE"],
+    "Polysexual": ["#F714BA", "#01D66A", "#1594F6"],
+    "Omnisexual": ["#FE9ACE", "#FF53BF", "#200044", "#6760FE", "#8EA6FF"],
+    "Non-Binary": ["#FCF434", "#FFFFFF", "#9C59D1", "#2C2C2C"]
 }
