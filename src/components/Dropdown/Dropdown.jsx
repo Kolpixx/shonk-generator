19830f59574c036f1975274b2c67e02c18164877
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2025-12-29 23:50:39 +0100

    collapse dropdown if user clicks outside or selects an option

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -11,11 +11,13 @@ export default function Dropdown({ options, state, setState }) {
                 <p onClick={() => showOptions(!showingOptions)}>{state} {showingOptions ? <ChevronUp size={32} strokeWidth={2} /> : <ChevronDown size={32} strokeWidth={2} />}</p>
             </div>
             {showingOptions && (
-                <div className="dropdown-options">
-                    {Object.keys(options).map((option, index) => <p className="dropdown-option pointer" key={`option-${index}`} onClick={() => setState(option)}>{option}</p>)}
-                </div>
+                <>
+                    <div className="dropdown-bg" onClick={(e) => {(e.target.id !== "dropdown-options" || "dropdown-selected") && showOptions(false) }}></div>
+                    <div className="dropdown-options">
+                        {Object.keys(options).map((option, index) => <p className="dropdown-option pointer" key={`option-${index}`} onClick={() => {setState(option); showOptions(false)}}>{option}</p>)}
+                    </div>
+                </>
             )}
-
         </div>
     )
 }
