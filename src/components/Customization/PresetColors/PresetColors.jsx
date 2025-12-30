87a103891e270505c815e5b058de59605fe09329
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2025-12-30 13:50:55 +0100

    add keys

--- src/components/Customization/PresetColors/PresetColors.jsx
+++ src/components/Customization/PresetColors/PresetColors.jsx
@@ -3,7 +3,7 @@ import './PresetColors.css'
 export default function PresetColors({ setColors, label, colors }) {
     return (
         <div title={label} className="preset-color-combination pointer" onClick={() => setColors(colors)}>
-            {colors.map(color => <div style={{backgroundColor: color}}></div> )}
+            {colors.map((color, index) => <div key={`color-${index}`} style={{backgroundColor: color}}></div> )}
         </div>
     )
 }
