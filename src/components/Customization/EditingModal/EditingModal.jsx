bb49efacfe32597c53121c0b5995f49da1417910
Kolpixx <68741965+Kolpixx@users.noreply.github.com>
2026-01-04 01:23:42 +0100

    disable wrap in textarea

--- src/components/Customization/EditingModal/EditingModal.jsx
+++ src/components/Customization/EditingModal/EditingModal.jsx
@@ -8,7 +8,7 @@ export default function EditingModal({ showEditingScreen, updateShonk }) {
         <div className="modal" onClick={(e) => {e.target.classList[0] === "modal" && showEditingScreen(false)}}>
             <div className="modal-container" id="editing-modal-container">
                 <h3>Enter text here :3</h3>
-                <textarea id="editing-custom-textarea" placeholder="Be silly :333"></textarea>
+                <textarea id="editing-custom-textarea" placeholder="Be silly :333" wrap="off"></textarea>
                 <button id="editing-custom-save-button" className="pointer" onClick={() => {setCustomVariant(document.getElementById("editing-custom-textarea").value); showEditingScreen(); updateShonk()}}><Save size={32} color={textColor} strokeWidth={1.75} /></button>
             </div>
         </div>
